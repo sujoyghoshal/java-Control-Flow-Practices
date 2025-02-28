841169a86e407bd02612003ba055204e2254738e
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:10:01 +0530

    [sujoy]des: Implement FizzBuzz using while loop

---
+++ FizzBuzzWhile.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FizzBuzzWhile {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Invalid input. Enter a positive integer.");
+            return;
+        }
+
+        int i = 1;
+        while (i <= number) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
