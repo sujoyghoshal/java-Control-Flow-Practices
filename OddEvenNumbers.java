94973fc999773a3fadbf791359f3af7caff67c6b
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:07:11 +0530

    [sujoy]des: Print odd and even numbers up to a given number

---
+++ OddEvenNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class OddEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Please enter a valid natural number.");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " is Even");
+            } else {
+                System.out.println(i + " is Odd");
+            }
+        }
+        scanner.close();
+    }
+}
