227afdfe1fe691740b5a418da114a6fa70a2b2d6
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:09:19 +0530

    [sujoy]des: Implement FizzBuzz using for loop

---
+++ FizzBuzzFor.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FizzBuzzFor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Invalid input. Enter a positive integer.");
+            return;
+        }
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
