0590a59e38470677563633766df07ac3998306ee
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:11:24 +0530

    [sujoy]des: Find factors of a number using for loop

---
+++ FactorsForLoop.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorsForLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = scanner.nextInt();
+
+        if (number < 1) {
+            System.out.println("Invalid input.");
+            return;
+        }
+
+        System.out.print("Factors of " + number + " are: ");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
