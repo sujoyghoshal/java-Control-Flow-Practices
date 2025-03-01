24c7d60e809611e99a9f557c9cf7050f849e65e8
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:41:34 +0530

    [sujoy]des: Implement Harshad Number check using while loop

---
+++ HarshadNumber.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        int originalNumber = number;
+        int sum = 0;
+
+        while (originalNumber != 0) {
+            sum += originalNumber % 10; 
+            originalNumber /= 10; 
+        }
+
+        if (number % sum == 0) {
+            System.out.println(number + " is a Harshad Number.");
+        } else {
+            System.out.println(number + " is NOT a Harshad Number.");
+        }
+    }
+}
