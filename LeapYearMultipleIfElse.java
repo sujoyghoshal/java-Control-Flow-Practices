261a223bc030780d0559a3318f595133273cd7e7
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:35:30 +0530

    [sujoy]des: Implement Leap Year check using multiple if-else statements

---
+++ LeapYearMultipleIfElse.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LeapYearMultipleIfElse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = scanner.nextInt();
+
+        if (year < 1582) {
+            System.out.println("Invalid input. Enter a year from 1582 onwards.");
+        } else {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0) {
+                        System.out.println(year + " is a Leap Year.");
+                    } else {
+                        System.out.println(year + " is NOT a Leap Year.");
+                    }
+                } else {
+                    System.out.println(year + " is a Leap Year.");
+                }
+            } else {
+                System.out.println(year + " is NOT a Leap Year.");
+            }
+        }
+    }
+}
