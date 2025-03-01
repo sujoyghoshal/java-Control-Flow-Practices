7d3d3a6b0f9898d599a1067bdb68804d20460755
sujoyghoshal <sujoyghoshal.s@gmail.com>
2025-03-01 05:48:26 +0530

    Added program to compute grade based on marks

---
+++ ComputeGrade.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ComputeGrade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter marks for Physics: ");
+        int physics = sc.nextInt();
+        System.out.print("Enter marks for Chemistry: ");
+        int chemistry = sc.nextInt();
+        System.out.print("Enter marks for Mathematics: ");
+        int mathematics = sc.nextInt();
+
+        double average = (physics + chemistry + mathematics) / 3.0;
+        String grade, remarks;
+
+        if (average >= 80) {
+            grade = "A";
+            remarks = "Level 4, above agency-normalized standards";
+        } else if (average >= 70) {
+            grade = "B";
+            remarks = "Level 3, at agency-normalized standards";
+        } else if (average >= 60) {
+            grade = "C";
+            remarks = "Level 2, below, but approaching agency-normalized standards";
+        } else if (average >= 50) {
+            grade = "D";
+            remarks = "Level 1, well below agency-normalized standards";
+        } else if (average >= 40) {
+            grade = "E";
+            remarks = "Level 1-, too below agency-normalized standards";
+        } else {
+            grade = "R";
+            remarks = "Remedial standards";
+        }
+
+        System.out.println("\nAverage Marks: " + average);
+        System.out.println("Grade: " + grade);
+        System.out.println("Remarks: " + remarks);
+    }
+}
